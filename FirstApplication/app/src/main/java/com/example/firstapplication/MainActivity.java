bab0f5d976eae19ef30497112fa8a385fd3d96e4
Mirella Lins <mirella.lds@gmail.com>
2020-07-23 22:26:22 -0300

    First commit

---
+++ FirstApplication/app/src/main/java/com/example/firstapplication/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.firstapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+//MVVM
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+    }
+}
