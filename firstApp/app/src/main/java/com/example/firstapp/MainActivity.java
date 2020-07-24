3674a02319357a512829271e19b4840e218d9b64
Rodrigo Coutelo <rodrigo@coutelo.me>
2020-07-23 23:57:24 -0300

    realizadao atividade sugeria aula 2

--- firstApp/app/src/main/java/com/example/firstapp/MainActivity.java
+++ firstApp/app/src/main/java/com/example/firstapp/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -12,11 +13,28 @@ public class MainActivity extends AppCompatActivity {
 
     private int total = 0;
 
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    final Button button = (Button)findViewById(R.id.button);
+
+        button.setOnClickListener (new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TextView mTvTotal = findViewById(R.id.tv_total);
+                mTvTotal.setText(R.string.text_setonclicklistener);
+                mTvTotal.setTextSize(35);
+                mTvTotal.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+            }
+        });
     }
+
+
+
+
 
     public void showToast(View view) {
         Toast.makeText(this, getString(R.string.toast_message), Toast.LENGTH_LONG).show();
